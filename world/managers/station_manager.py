75bf831c945a83b019ae3776fca5d1b5dc8b2b64
h23881956 <h23881956@gmail.com>
2025-03-02 23:06:17 +0800

    發佈版本 v0.1.1：修復了stations_by_id 字典錯誤使用的情況

--- world/managers/station_manager.py
+++ world/managers/station_manager.py
@@ -29,7 +29,7 @@ class StationManager:
         return self.stations_by_id[station_id]
     
     def addStation(self, station: Station):
-        self.stations_by_id[station.station_id] = station
+        self.stations_by_id[station.id] = station
 
         if station.isPickerStation():
             self.picking_stations.append(station)
@@ -94,25 +94,22 @@ class StationManager:
         # Check if more than one station is available
         if len(available_station) > 1:
             for station in available_station:
-                # Check Available Station
-                similarity_score = 0
-                if len(station.order_ids) < station.max_orders:
-                    # Take pod assigned to this particular station
-                    station_incoming_pod = station.incoming_pod
-                    station_pod_skus_set = set()
-                    for pod_id in station_incoming_pod:
-                        pod  = pod_manager.getPodByNumber(pod_id)
-                        pod_skus = [item for item, details in pod.skus.items() if details['current_qty'] > 0]
-                        station_pod_skus_set.update(pod_skus)
+                # Take pod assigned to this particular station
+                station_incoming_pod = station.incoming_pod
+                station_pod_skus_set = set()
+                for pod_id in station_incoming_pod:
+                    pod  = pod_manager.getPodByNumber(pod_id)
+                    pod_skus = [item for item, details in pod.skus.items() if details['current_qty'] > 0]
+                    station_pod_skus_set.update(pod_skus)
 
-                    station_pod_skus_list = list(station_pod_skus_set)
-                    station_pod_skus_in_order_mask = np.isin(sku_in_order_list, station_pod_skus_list)
-                    station_pod_skus_in_order = np.array(sku_in_order_list)[station_pod_skus_in_order_mask]
-                    similarity_score = len(station_pod_skus_in_order)
+                station_pod_skus_list = list(station_pod_skus_set)
+                station_pod_skus_in_order_mask = np.isin(sku_in_order_list, station_pod_skus_list)
+                station_pod_skus_in_order = np.array(sku_in_order_list)[station_pod_skus_in_order_mask]
+                similarity_score = len(station_pod_skus_in_order)
 
-                    available_station_rank = pd.concat([available_station_rank , 
-                                                pd.DataFrame([[station.station_id, similarity_score]], columns=["station_id", "similarity_score"])], ignore_index=True) 
-            
+                available_station_rank = pd.concat([available_station_rank , 
+                                            pd.DataFrame([[station.id, similarity_score]], columns=["station_id", "similarity_score"])], ignore_index=True) 
+        
             available_station_rank.sort_values(by=["similarity_score"], ascending=False, inplace=True)
             available_station_rank.reset_index(drop=True, inplace=True)
 
